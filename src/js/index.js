a30dd667fb39033b8aa15dcbde29ebf337bce6f6
minupalaniappan <minu@middesk.com>
2021-04-10 13:46:00 -0600

    Fix load

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,11 @@ import getSelection from './listener'
 import { execute } from './execute'
 
 const app = () => {
-  getSelection((e) => execute(e.toString().trim()))
+  getSelection((e) => {
+    execute(e.toString().trim())
+  })
 }
 
 window.addEventListener('load', () => {
   app()
-  execute('Bradley Beal')
 })
