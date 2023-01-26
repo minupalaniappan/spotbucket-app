2496af33eab6728a9b9976b3b019ef1e94afe32e
Minu Palaniappan <minupal6@gmail.com>
2023-01-25 22:45:26 -0500

    done

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,64 @@
-var webpack = require('webpack'),
-  path = require('path'),
-  fileSystem = require('fs'),
-  env = require('./utils/env'),
-  CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin,
-  CopyWebpackPlugin = require('copy-webpack-plugin'),
-  HtmlWebpackPlugin = require('html-webpack-plugin'),
-  WriteFilePlugin = require('write-file-webpack-plugin')
+var webpack = require("webpack"),
+  path = require("path"),
+  fileSystem = require("fs"),
+  env = require("./utils/env"),
+  CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin,
+  CopyWebpackPlugin = require("copy-webpack-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  WriteFilePlugin = require("write-file-webpack-plugin");
 
 // load the secrets
-var alias = {}
+var alias = {};
 
-var secretsPath = path.join(__dirname, 'secrets.' + env.NODE_ENV + '.js')
+var secretsPath = path.join(__dirname, "secrets." + env.NODE_ENV + ".js");
 
 var fileExtensions = [
-  'jpg',
-  'jpeg',
-  'png',
-  'gif',
-  'eot',
-  'otf',
-  'svg',
-  'ttf',
-  'woff',
-  'woff2',
-]
+  "jpg",
+  "jpeg",
+  "png",
+  "gif",
+  "eot",
+  "otf",
+  "svg",
+  "ttf",
+  "woff",
+  "woff2",
+];
 
 if (fileSystem.existsSync(secretsPath)) {
-  alias['secrets'] = secretsPath
+  alias["secrets"] = secretsPath;
 }
 
 var options = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: process.env.NODE_ENV || "development",
   entry: {
-    index: path.join(__dirname, 'src', 'js', 'index.js'),
-    inject: path.join(__dirname, 'src', 'js', 'inject.js'),
+    index: path.join(__dirname, "src", "js", "index.js"),
+    inject: path.join(__dirname, "src", "js", "inject.js"),
   },
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].[chunkhash].js',
+    path: path.join(__dirname, "build"),
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].[chunkhash].js",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader',
+        loader: "style-loader!css-loader",
       },
       {
-        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
-        loader: 'file-loader?name=[name].[ext]',
+        test: new RegExp(".(" + fileExtensions.join("|") + ")$"),
+        loader: "file-loader?name=[name].[ext]",
         exclude: /node_modules/,
       },
       {
         test: /\.html$/,
-        loader: 'html-loader',
+        loader: "html-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/,
       },
     ],
@@ -66,17 +66,17 @@ var options = {
   resolve: {
     alias: alias,
     extensions: fileExtensions
-      .map((extension) => '.' + extension)
-      .concat(['.jsx', '.js', '.css']),
+      .map((extension) => "." + extension)
+      .concat([".jsx", ".js", ".css"]),
   },
   plugins: [
     // clean the build folder
     new CleanWebpackPlugin(),
     // expose and write the allowed env vars on the compiled bundle
-    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new webpack.EnvironmentPlugin(["NODE_ENV"]),
     new CopyWebpackPlugin([
       {
-        from: 'src/manifest.json',
+        from: "src/manifest.json",
         transform: function (content, path) {
           // generates the manifest file using the package.json informations
           return Buffer.from(
@@ -85,16 +85,16 @@ var options = {
               version: process.env.npm_package_version,
               ...JSON.parse(content.toString()),
             })
-          )
+          );
         },
       },
     ]),
     new WriteFilePlugin(),
   ],
+};
+
+if (env.NODE_ENV === "development") {
+  options.devtool = "cheap-module-source-map";
 }
 
-if (env.NODE_ENV === 'development') {
-  options.devtool = 'cheap-module-eval-source-map'
-}
-
-module.exports = options
+module.exports = options;
